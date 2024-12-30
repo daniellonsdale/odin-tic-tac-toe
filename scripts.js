32540051908e7bd6c37470e3992ed24001447f0d
daniellonsdale <danielllonsdale@gmail.com>
2024-12-30 21:25:26 +0900

    Implement logic to assign players x/y based one p1 prompt respose

--- scripts.js
+++ scripts.js
@@ -8,6 +8,17 @@ const gameBoard = (function (){
 
     const players = [];
     const addPlayer = (player) => {
+        if(players.length === 0){
+            let playerChoice = prompt("Choose your piece: x or o");
+            console.log(`Player one will be ${playerChoice}`);
+            player.playerPiece = playerChoice;
+        }else if(players.length === 1 && players[0].playerPiece === 'x'){
+            console.log('Player two will be o')
+            player.playerPiece = 'o';
+        }else if(players.length === 1 && players[0].playerPiece === 'o'){
+            console.log('Player two will be x')
+            player.playerPiece = 'x';
+        }
         if(players.length < 2){
             players.push(player);
             console.log(`${player} added to ${players}`);
@@ -32,3 +43,8 @@ function createPlayer(name){
 
     return {getPlayerName, playerPiece};
 }
+
+const playerOne = createPlayer('Player one');
+gameBoard.addPlayer(playerOne);
+const playerTwo = createPlayer('Player two');
+gameBoard.addPlayer(playerTwo);
